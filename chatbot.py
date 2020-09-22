cd8be994ad5422b79eed0410c2a1f8f530359402
JayWasTaken <jaymay8k@gmail.com>
2020-09-22 15:10:06 -0700

    added info/context

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
+# this will get a random choice from each list
 from random import choice
-
+# list what reccommendations the bot has for user input
 def get_bot_reccommend(response):
 
     bot_reccommend_mellow = ["You probably want to listen to mellow vibes for this weather.", "Maybe listen to some Instrumental Blues music.", "Chillhop is a great playlist option for this!"]
@@ -16,7 +17,8 @@ def get_bot_reccommend(response):
         return "For that, i dont have a playlist reccommendation. Youre on your own, sorry."
 
 print("This program's going to tell you what mood music you need.")
-
+# this is the loop to make sure it'll ask a lot so i dont have to run it a lot
+# itll also show everything that repeats: the question, user response, and the bot reccomendation
 while True:
 
     response = input("So whats the weather? ")
