3248a80c76d36f5dc38d5f802137a8652b1eff33
JayWasTaken <jaymay8k@gmail.com>
2020-09-22 18:37:41 -0700

    corrected spelling

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Weather Playlist Reccomendation Bot
+Weather Playlist Reccommendation Bot
 
-This chatbot is one i thought would make in order to reccomend some music based on the weather. 
+This chatbot is one I thought I would make in order to reccommend some music based on the weather. 
 
 The bot has a list of several different kinds of weather with a sub-section saying what keywords will work when the user types in a response.
 
-When the program is run, it will display what it does in a short summary. Then, within a while loop, the bot will ask the user what the weather is. Then will repeat the question, output user response, and reccomendation for that response until the user types 'done' which will end the loop and stop running the program.
+When the program is run, it will display what it does in a short summary. Then, within a while loop, the bot will ask the user what the weather is. Then will repeat the question, output user response, and reccommendation for that response until the user types 'done' which will end the loop and stop running the program.
--- chatbot.py
+++ chatbot.py
@@ -3,9 +3,9 @@ from random import choice
 # list what reccommendations the bot has for user input
 def get_bot_reccommend(response):
 
-    bot_reccommend_mellow = ["You probably want to listen to mellow vibes for this weather.", "Maybe listen to some Instrumental Blues music.", "Chillhop is a great playlist option for this!", "Nature sounds are quite peaceful."]
+    bot_reccommend_mellow = ["You probably want to listen to mellow vibes for this weather.", "Maybe listen to some Instrumental Blues music.", "Chillhop is a great playlist option for this!", "Nature sounds are quite peaceful this time of year."]
     bot_reccommend_bright = ["You probably want to listen to some bright happy vibes, its a great day!", "Come on, you know you want to listen to some Disney music.", "Some fun Pop music would be great!", "Just play He-Man singing 'What's goin on' on repeat."]
-    bot_reccommend_edgy = ["You def need something edgy for this, I mean look outside!", "I reccommend some Rock music.", "Electro is a must for this kind of weather.", "Listen to a little metal. Not too much tho."]
+    bot_reccommend_edgy = ["You def need something edgy for this, I mean look outside!", "I reccommend some Rock music.", "Electro is a must for this kind of weather.", "Listen to a little Metal. Not too much tho."]
 
     if response in ["snow", "snowy", "nice", "rainy", "foggy", "calm"]:
         return choice(bot_reccommend_mellow)
@@ -15,15 +15,15 @@ def get_bot_reccommend(response):
         return choice(bot_reccommend_edgy)
     else:
         return "For that, i dont have a playlist reccommendation. Youre on your own, sorry."
-
-print("This program's going to reccommend music based off the weather, to meet all your vibin needs.")
-print("It's got a reccomendation for almost anything, from sunny to freezing.")
+# this is the bot intro, outside the loop so it doesnt repeat
+print("This program's going to reccommend music based off the weather, to meet all your vibin needs!")
+print("It's got a reccommendation for almost anything, from sunny to freezing.")
 print("Whenever you're finished with the program, just type 'done'.")
 # this is the loop to make sure it'll ask a lot so i dont have to run it a lot
-# itll also show everything that repeats: the question, user response, and the bot reccomendation
+# itll also show everything that repeats: the question, user response, and the bot reccommendation
 while True:
 
-    response = input("So whats the weather? ")
+    response = input("So what's the weather? ")
 
     if response == "done":
         break
