26ad9bdb05f8b762d6a1ddf0b2215f2dc996e690
JayWasTaken <jaymay8k@gmail.com>
2020-09-22 18:22:31 -0700

    added list options

--- chatbot.py
+++ chatbot.py
@@ -3,20 +3,22 @@ from random import choice
 # list what reccommendations the bot has for user input
 def get_bot_reccommend(response):
 
-    bot_reccommend_mellow = ["You probably want to listen to mellow vibes for this weather.", "Maybe listen to some Instrumental Blues music.", "Chillhop is a great playlist option for this!"]
-    bot_reccommend_bright = ["You probably want to listen to some bright happy vibes, its a great day!", "Come on, you know you want to listen to some Disney music", "Some fun Pop music would be great!"]
-    bot_reccommend_edgy = ["You def need something edgy for this, I mean look outside!", "I reccommend some Rock music.","Electro is a must for this kind of weather."]
+    bot_reccommend_mellow = ["You probably want to listen to mellow vibes for this weather.", "Maybe listen to some Instrumental Blues music.", "Chillhop is a great playlist option for this!", "Nature sounds are quite peaceful."]
+    bot_reccommend_bright = ["You probably want to listen to some bright happy vibes, its a great day!", "Come on, you know you want to listen to some Disney music.", "Some fun Pop music would be great!", "Just play He-Man singing 'What's goin on' on repeat."]
+    bot_reccommend_edgy = ["You def need something edgy for this, I mean look outside!", "I reccommend some Rock music.", "Electro is a must for this kind of weather.", "Listen to a little metal. Not too much tho."]
 
-    if response in ["snowing", "rainy", "foggy"]:
+    if response in ["snow", "snowy", "nice", "rainy", "foggy", "calm"]:
         return choice(bot_reccommend_mellow)
-    elif response in ["sunny", "crisp", "breezy"]:
+    elif response in ["sunny", "crisp", "breezy", "clear", "great", "warm"]:
         return choice(bot_reccommend_bright)
-    elif response in ["windy", "freezing", "cloudy"]:
+    elif response in ["windy", "freezing", "cloudy", "bad", "stormy", "hail"]:
         return choice(bot_reccommend_edgy)
     else:
         return "For that, i dont have a playlist reccommendation. Youre on your own, sorry."
 
-print("This program's going to tell you what mood music you need.")
+print("This program's going to reccommend music based off the weather, to meet all your vibin needs.")
+print("It's got a reccomendation for almost anything, from sunny to freezing.")
+print("Whenever you're finished with the program, just type 'done'.")
 # this is the loop to make sure it'll ask a lot so i dont have to run it a lot
 # itll also show everything that repeats: the question, user response, and the bot reccomendation
 while True:
